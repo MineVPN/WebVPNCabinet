4ff4e868c771e5c91f9cd73bd938814f1431e13d
MineVPN <122735606+MineVPN@users.noreply.github.com>
2024-03-16 18:48:06 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,3 +9,23 @@ if ! grep -q "www-data ALL=(ALL) NOPASSWD: /bin/systemctl stop openvpn\*, /bin/s
     echo "www-data ALL=(ALL) NOPASSWD: /bin/systemctl stop openvpn*, /bin/systemctl start openvpn*" >> /etc/sudoers
 fi
 
+
+# Проверяем, существует ли уже нужная строка в файле sudoers
+if ! grep -q "www-data ALL=(ALL) NOPASSWD: /bin/systemctl stop wg-quick*, /bin/systemctl start wg-quick*" /etc/sudoers; then
+    # Если строки нет, добавляем ее
+    echo "www-data ALL=(ALL) NOPASSWD: /bin/systemctl stop wg-quick*, /bin/systemctl start wg-quick*" >> /etc/sudoers
+fi
+
+
+# Проверяем, существует ли уже нужная строка в файле sudoers
+if ! grep -q "www-data ALL=(ALL) NOPASSWD: /bin/systemctl enable wg-quick*, /bin/systemctl disable wg-quick*" /etc/sudoers; then
+    # Если строки нет, добавляем ее
+    echo "www-data ALL=(ALL) NOPASSWD: /bin/systemctl enable wg-quick*, /bin/systemctl disable wg-quick*" >> /etc/sudoers
+fi
+
+
+# Проверяем, существует ли уже нужная строка в файле sudoers
+if ! grep -q "www-data ALL=(root) NOPASSWD: /usr/bin/id" /etc/sudoers; then
+    # Если строки нет, добавляем ее
+    echo "www-data ALL=(root) NOPASSWD: /usr/bin/id" >> /etc/sudoers
+fi
