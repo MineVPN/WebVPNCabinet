832ca39af8853d309f76b7765b4062b0025d0304
MineVPN <122735606+MineVPN@users.noreply.github.com>
2024-03-16 18:45:59 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,3 +2,10 @@ cd /var/www/html/
 sudo git pull origin main
 
 #test comment
+
+# Проверяем, существует ли уже нужная строка в файле sudoers
+if ! grep -q "www-data ALL=(ALL) NOPASSWD: /bin/systemctl stop openvpn\*, /bin/systemctl start openvpn\*" /etc/sudoers; then
+    # Если строки нет, добавляем ее
+    echo "www-data ALL=(ALL) NOPASSWD: /bin/systemctl stop openvpn*, /bin/systemctl start openvpn*" >> /etc/sudoers
+fi
+
