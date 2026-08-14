cb0070343f94d246fee875b9771d5d3fb794a36b
MineVPN <122735606+MineVPN@users.noreply.github.com>
2026-08-14 12:57:54 +0000

    Update vpn-manager.php

--- pages/vpn-manager.php
+++ pages/vpn-manager.php
@@ -298,13 +298,6 @@ window.__flashMessage = {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/>
                     </svg>
                 </div>
-                <div class="vpn-single-config-warning-body">
-                    <div class="vpn-single-config-warning-title">Резервирования нет</div>
-                    <div class="vpn-single-config-warning-text">
-                        У вас только один конфиг. Если он перестанет работать, локальная сеть
-                        останется без интернета. Стоит добавить запасной.
-                    </div>
-                </div>
             </div>
         <?php endif; ?>
 
